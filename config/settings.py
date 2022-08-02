539b5394c04373eeb0fa588df7d0e9e94cae9b38
hossein ot <boyyoung2020@gmail.com>
2022-08-02 13:13:18 +0430

    secend commit

--- config/settings.py
+++ config/settings.py
@@ -135,7 +135,6 @@ AUTH_USER_MODEL = 'account.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        # 'rest_framework.permissions.IsAuthenticated',
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
